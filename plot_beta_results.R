18071d8fac33a7ddb5b0c37893d2e6d314081d55
qinglong-tian <qltian2021@gmail.com>
2022-06-15 20:52:43 -0500

    remove old beta function and rename the new beta function

--- plot_beta_results.R
+++ plot_beta_results.R
@@ -103,7 +103,7 @@ datBeta %>% select(
   ),
   Type = ifelse(
     variable %in% c("b1effse", "b2effse"),
-    "Propsed (Empirical)",
+    "Proposed (Empirical)",
     ifelse(
       variable %in% c("b1naivese", "b2naivese"),
       "Naive (Empirical)",
@@ -116,7 +116,7 @@ datBeta %>% select(
   )
 ) %>% mutate(
   Ratio = factor(Ratio, levels = c('m/n*"=0.5"', 'm/n*"=1"', 'm/n*"=1.5"')),
-  Type = factor(Type, levels = c("Propsed (Empirical)", "Naive (Empirical)", "Perturbation (Estimated)", "Formula (Estimated)"))
+  Type = factor(Type, levels = c("Proposed (Empirical)", "Naive (Empirical)", "Perturbation (Estimated)", "Formula (Estimated)"))
 ) %>% ggplot(
   aes(x = n, y = SE, col = Type)
 ) + geom_line(
