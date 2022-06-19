28eceeccd3d9db8593c5a6d314d4fbb15b8a15b7
qinglong-tian <qltian2021@gmail.com>
2022-06-19 11:51:21 -0500

    add simu code for hypothesis testing

---
+++ simu_test.R
@@ -0,0 +1,12 @@
+library(parallel)
+library(Rcpp)
+library(fastGHQuad)
+source("data_generating_functions.R")
+sourceCpp("fast_estimation_functions.cpp")
+#############################################
+# Factor
+
+n <- 1000
+mnratio <- 
+
+#############################################
