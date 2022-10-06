9c3051c89f48349ee3265f3175414aa6d03ddceb
qinglong-tian <qltian2021@gmail.com>
2022-10-05 21:03:38 -0500

    multinominal

--- application_func.R
+++ application_func.R
@@ -1,5 +1,3 @@
-library(tidyverse)
-
 Compute_Marginal_of_SOFA <- function(sData, tData)
 {
   tSofa <- unique(tData$sofa)
@@ -55,3 +53,146 @@ Compute_GLM_Pr_R1_Given_Y <- function(sData, tData, rSofaLogistic)
   
   return(dff)
 }
+
+Fit_Sofa_Score <- function(sDat)
+{
+  sDat %>% mutate(sofa = factor(sofa, levels = paste(0:10))) %>% mutate (sofa = relevel(sofa, ref = "10")) -> sDat1
+  multinom_model <- multinom(sofa ~ ., data = sDat1)
+  return(multinom_model)
+}
+
+E_S_RHO_App <- function(betaVal, sDat)
+{
+  sofaScore <- sDat$sofa
+  mean(exp(betaVal * sofaScore))
+}
+
+Pred_Sofa_Given_X_S <- function(sDat, tDat)
+{
+  multi_nominal <- Fit_Sofa_Score(sDat)
+  prob_xs <- predict(multi_nominal, newdata = sDat, type = "probs")
+  prob_xt <- predict(multi_nominal, newdata = tDat, type = "probs")
+  
+  return(list(prob_list_xs = prob_xs,
+              prob_list_xt = prob_xt))
+}
+
+E_S_RHO_Given_X_App <- function(betaVal, prob_list_x, pwr)
+{
+  matrix(
+    exp(pwr * betaVal * (0:10)),
+    ncol = length(0:10),
+    nrow = nrow(prob_list_x),
+    byrow = T
+  ) -> rhoMat
+  rowSums(rhoMat * prob_list_x)
+}
+
+E_T_RHO_Given_X_App <- function(betaVal, prob_list_x)
+{
+  es2 <- E_S_RHO_Given_X_App(betaVal, prob_list_x, 2)
+  es <- E_S_RHO_Given_X_App(betaVal, prob_list_x, 1)
+  
+  es2 / es
+}
+
+Compute_Tau_App <- function(betaVal, prob_list_x, c_ps, piVal)
+{
+  e_t_rho_given_x <- E_T_RHO_Given_X_App(betaVal, prob_list_x)
+  tmp <- e_t_rho_given_x / piVal / c_ps
+  
+  tmp / (tmp + 1 / (1 - piVal))
+}
+
+E_S_RHO_Y_Given_X_App <- function(betaVal, prob_list_x)
+{
+  matrix((0:10) * exp((0:10) * betaVal),
+         ncol = length(0:10),
+         nrow = nrow(prob_list_x),
+         byrow = T
+  ) -> rhoMat
+  
+  rowSums(rhoMat * prob_list_x)
+}
+
+E_S_RHO_Y_App <- function(betaVal, sDat)
+{
+  sofaScore <- sDat$sofa
+  rhoScore <- exp(sofaScore * betaVal)
+  mean(sofaScore * rhoScore)
+}
+
+Compute_S_App <- function(betaVal, prob_list_x, sDat, c_ps)
+{
+  e_s_rho_y_given_x <- E_S_RHO_Y_Given_X_App(betaVal, prob_list_x)
+  e_s_rho_given_x <- E_S_RHO_Given_X_App(betaVal, prob_list_x, 1)
+  e_s_rho_y <- E_S_RHO_Y_App(betaVal, sDat)
+  
+  e_s_rho_y_given_x / e_s_rho_given_x - e_s_rho_y / c_ps
+}
+
+E_T_Tau_S_App <- function(betaVal, prob_list_xt, sDat, c_ps, piVal)
+{
+  SVec <- Compute_S_App(betaVal, prob_list_xt, sDat, c_ps)
+  rhoVec <- Compute_Tau_App(betaVal, prob_list_xt, c_ps, piVal)
+  mean(SVec * rhoVec)
+}
+
+E_T_Tau_App <- function(betaVal, prob_list_xt, c_ps, piVal)
+{
+  mean(Compute_Tau_App(betaVal, prob_list_xt, c_ps, piVal))
+}
+
+Compute_B_App <-
+  function(betaVal,
+           prob_list_x,
+           prob_list_xt,
+           sDat,
+           c_ps,
+           piVal)
+  {
+    tau_x <- Compute_Tau_App(betaVal, prob_list_x, c_ps, piVal)
+    s_x <- Compute_S_App(betaVal, prob_list_x, sDat, c_ps)
+    e_t_tau_s <-
+      E_T_Tau_S_App(betaVal, prob_list_xt, sDat, c_ps, piVal)
+    e_t_tau <- E_T_Tau_App(betaVal, prob_list_xt, c_ps, piVal)
+    (-1) * (1 - piVal) * (1 - tau_x) * (s_x - e_t_tau_s / (e_t_tau - 1))
+  }
+
+Compute_S_Eff_App <-
+  function(betaVal,
+           sDat,
+           c_ps,
+           piVal,
+           prob_list_xs,
+           prob_list_xt)
+  {
+    c_ps <- E_S_RHO_App(betaVal, sDat)
+    sofaScore <- sDat$sofa
+    rhoVec <- exp(betaVal * sofaScore)
+    
+    mult1 <- 1 / piVal * rhoVec / c_ps
+    b1 <-
+      Compute_B_App(betaVal, prob_list_xs, prob_list_xt, sDat, c_ps, piVal)
+    
+    mult2 <- -1 / (1 - piVal)
+    b2 <-
+      Compute_B_App(betaVal, prob_list_xt, prob_list_xt, sDat, c_ps, piVal)
+    
+    S_Eff <- c(mult1 * b1, mult2 * b2)
+    
+    return(as.numeric(S_Eff))
+  }
+
+Compute_S_Eff_Sum_App <-
+  function(betaVal,
+           sDat,
+           piVal,
+           prob_list_xs,
+           prob_list_xt)
+  {
+    c_ps <- E_S_RHO_App(betaVal, sDat)
+    S_Eff <-
+      Compute_S_Eff_App(betaVal, sDat, c_ps, piVal, prob_list_xs, prob_list_xt)
+    mean(S_Eff) ^ 2
+  }
--- applications.R
+++ applications.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(reshape2)
+library(nnet)
+
 source("application_func.R")
 dat_dir <- "real_dat"
 dat3 <- readr::read_csv(paste(dat_dir, "/data3.csv", sep = ""))
@@ -30,13 +32,14 @@ data <-
   ) %>% mutate(admittime = as.Date(admittime, "%m/%d/%Y")) %>% filter(age < 65,
                                                                       sofa <= 10) %>% group_by(subject_id) %>% slice(which.min(admittime)) %>% na.omit %>% mutate(R = if_else(insurance %in% c("Medicare", "Medicaid"), 0, 1))
 
-sDat <- data %>% filter(R == 1)
-tDat <- data %>% filter(R == 0)
-
 rSofaLogistic <-
   glm(R ~ poly(sofa, 2),
       family = binomial(link = "logit"),
       data = data)
+
+sDat <- data %>% filter(R == 1) %>% select(-R,-insurance)
+tDat <- data %>% filter(R == 0) %>% select(-R,-insurance)
+
 Compute_GLM_Pr_R1_Given_Y(sDat, tDat, rSofaLogistic) -> df
 df <- df %>% mutate(SOFA = as.numeric(SOFA))
 df %>% melt(
@@ -45,3 +48,26 @@ df %>% melt(
   value.name = "Prob"
 ) -> df
 df %>% ggplot(aes(x = SOFA, y = Prob, col = Type)) + geom_point() + geom_line()
+
+#
+# sDat %>% mutate(sofa = factor(sofa, levels = paste(0:10))) %>% mutate(sofa = relevel(sofa, ref = "0")) -> sDat1
+# multinom_model <- multinom(sofa ~ ., data = sDat1)
+# predict(multinom_model, newdata = sDat[1:5, ], type = "probs") -> probMat
+# apply(probMat, 1, cumsum)
+
+Pred_Sofa_Given_X_S(sDat, tDat) -> probList
+probListXs <- probList$prob_list_xs
+probListXt <- probList$prob_list_xt
+piVal <- nrow(sDat) / (nrow(sDat) + nrow(tDat))
+
+optim(
+  0,
+  Compute_S_Eff_Sum_App,
+  sDat = sDat,
+  piVal = piVal,
+  prob_list_xs = probListXs,
+  prob_list_xt = probListXt,
+  method = "Brent",
+  lower = -5,
+  upper = 5
+)
